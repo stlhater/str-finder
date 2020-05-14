b518d6d2daa8a878cdbad7865e04a80bdd72b920
stlhater <64039721+stlhater@users.noreply.github.com>
2020-05-14 19:03:06 +0300

    initial commit
    added project files

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+int find_str_entry(std::string in, std::string str) {
+	int goods = 0;
+	int counter = 0;
+	for (int i = 0, j = 0, len = in.length(); i < len; i++) {
+		if (str[j++] == in[i]) {
+			goods++;
+			if (goods == str.length()) {
+				return i - str.length() + 1;
+			}
+		}
+		else {
+			i = ++counter;
+			j = 0;
+			goods = 0;
+		}
+	}
+	return -1;
+}
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	std::string in = "Гуси по небу летели";
+	std::cout << "base string: " << in << std::endl;
+	std::cout << "entry of 'небу': " << find_str_entry(in, "небу") << std::endl;
+	std::cout << "entry of 'андрей': " << find_str_entry(in, "андрей") << std::endl;
+	std::cin.get();
+}
